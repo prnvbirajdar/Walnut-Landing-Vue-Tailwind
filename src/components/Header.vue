6b4765acd8960bea232e145f08e366e5f596df23
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-23 18:02:06 -0400

    header working and image working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="mt-28 w-11/12 md:w-10/12 mx-auto flex flex-col lg:flex-row">
+  <header class="mt-28 w-11/12 md:w-10/12 mx-auto flex flex-col lg:flex-row md:h-screen">
      <div class="mb-16 hidden md:block lg:hidden">
       <img class="object-cover h-96" src="/phone.png" alt="Phone Image" />
     </div>
@@ -8,7 +8,7 @@
         <h1 class="text-salmon font-bold text-4xl sm:text-5xl">
           Life insurance — now a premium membership you'll love
         </h1>
-        <p class="mt-4 text-lg text-gray-600">
+        <p class="mt-6 text-lg text-gray-600">
           Introducing a new era of life insurance. Affordable plans, instant
           coverage and exciting member benefits.
         </p>
@@ -34,40 +34,4 @@
       <img class="object-cover h-96 md:h-96 lg:h-auto" src="/phone.png" alt="Phone Image" />
     </div>
   </header>
-  <!-- <div
-    class="relative pt-16 pb-32 flex flex-col md:flex-row"
-    style="min-height: 75vh"
-  >
-    <div class="container relative mx-auto">
-      <div class="items-center flex flex-wrap">
-        <div class="w-full px-4 ml-auto mr-auto">
-          <div class="pr-12">
-            <h1 class="text-salmon font-medium text-4xl md:text-5xl">
-              Life insurance — now a premium membership you'll love
-            </h1>
-            <p class="mt-4 text-lg text-gray-400">
-              Introducing a new era of life insurance. Affordable plans, instant
-              coverage and exciting member benefits.
-            </p>
-          </div>
-          <div class="text-center mt-6">
-            <input
-              class="border w-full md:w-1/2 text-white px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1"
-              style="transition: all 0.15s ease 0s"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div class="text-center mt-6">
-            <button
-              class="bg-new-blue hover:bg-new-blue-hover transition-colors text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              Send Message
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div><img src="/phone.png" alt="Phone Image" /></div>
-  </div> -->
 </template>
