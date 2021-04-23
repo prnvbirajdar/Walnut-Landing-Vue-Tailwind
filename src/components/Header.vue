6e6ede105130d80f5f08e29263f41d88784c9ac9
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-23 16:25:08 -0400

    initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- <header class="mt-32 w-11/12 mx-auto flex md:flex-row">
+    <div>
+      <h1 class="text-[#f93b55] font-semibold text-4xl">
+        Life insurance — now a premium membership you'll love
+      </h1>
+      <p class="mt-4">
+        Introducing a new era of life insurance. Affordable plans, instant
+        coverage and exciting member benefits.
+      </p>
+      <div class="text-center mt-6">
+        <input
+          class=" border w-full text-white px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1"
+          style="transition: all 0.15s ease 0s;"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div class="text-center mt-6">
+        <button
+          class="bg-[#334dc6] w-full md:w-1/2 text-white active:bg-gray-700 px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+          type="button"
+          style="transition: all 0.15s ease 0s;"
+        >
+          Join Waitlist
+        </button>
+      </div>
+    </div>
+    <div><img src="/phone.png" alt="Phone Image" /></div>
+  </header> -->
+  <div
+    class="relative pt-16 pb-32 flex content-center items-center justify-center flex-col md:flex-row"
+    style="min-height: 75vh;"
+  >
+    <div class="container relative mx-auto">
+      <div class="items-center flex flex-wrap">
+        <div class="w-full px-4 ml-auto mr-auto text-center">
+          <div class="pr-12">
+            <h1 class="text-black font-semibold text-5xl md:text-[#f93]">
+              Life insurance — now a premium membership you'll love
+            </h1>
+            <p class="mt-4 text-lg">
+              Introducing a new era of life insurance. Affordable plans, instant
+              coverage and exciting member benefits.
+            </p>
+          </div>
+          <div class="text-center mt-6">
+            <input
+              class=" border w-full md:w-1/2 text-white px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1"
+              style="transition: all 0.15s ease 0s;"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div class="text-center mt-6">
+            <button
+              class="bg-[#334dc6] text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+              type="button"
+              style="transition: all 0.15s ease 0s;"
+            >
+              Send Message
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div><img src="/phone.png" alt="Phone Image" /></div>
+  </div>
+</template>
