a393ea54d4c9909dc1cc950a2d7a126c147646ad
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-23 17:10:20 -0400

    header working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "new-blue": "#334dc6",
+        "new-blue-hover": "#233589",
+        salmon: "#f93b55",
+        "green-new": "#288362",
+        gray: "#5d5d5d",
+        "dark-gray": "#353535",
+      },
+    },
   },
   variants: {
     extend: {},
